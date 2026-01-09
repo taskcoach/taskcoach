9a3aff07b4404996d504b0961127a91cd612acd1
Real Carbonneau <contact@realcarbonneau.com>
2026-01-08 21:18:36 -0500

    Replace win32 WNDPROC with native wxPython power events
    The win32 window procedure subclassing was causing access violations
    on exit because the Python callback could be invoked after the C++
    window object was destroyed.
    
    Now using wx.EVT_POWER_SUSPENDED and wx.EVT_POWER_RESUME which are
    wxPython's native power event handlers. This is safer because wxPython
    handles all the lifecycle management automatically.

--- taskcoachlib/powermgt/win32.py
+++ taskcoachlib/powermgt/win32.py
@@ -17,47 +17,29 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from taskcoachlib.powermgt.base import PowerStateMixinBase
-
-# pylint: disable=F0401
-import win32api
-import win32gui
-import win32con
 import wx
 
 
 class PowerStateMixin(PowerStateMixinBase):
+    """Windows power state handling using native wxPython events.
+
+    Uses wx.EVT_POWER_SUSPENDED and wx.EVT_POWER_RESUME instead of
+    win32 WNDPROC subclassing to avoid crashes on application exit.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Store handle now - can't call GetHandle() later when window is being destroyed
-        self.__hWnd = self.GetHandle()
-        self.__destroyed = False
-        self.__oldProc = win32gui.SetWindowLong(
-            self.__hWnd, win32con.GWL_WNDPROC, self.__WndProc
-        )
+        # Bind wxPython's native power events
+        self.Bind(wx.EVT_POWER_SUSPENDED, self.__onPowerSuspended)
+        self.Bind(wx.EVT_POWER_RESUME, self.__onPowerResume)
 
-    def __WndProc(self, hWnd, msg, wParam, lParam):
-        # After WM_DESTROY, stop all processing to avoid access violations
-        if self.__destroyed:
-            return win32gui.DefWindowProc(hWnd, msg, wParam, lParam)
+    def __onPowerSuspended(self, event):
+        """Handle system suspend event."""
+        self.OnPowerState(self.POWEROFF)
+        event.Skip()
 
-        if msg == win32con.WM_DESTROY:
-            self.__destroyed = True
-            try:
-                win32api.SetWindowLong(
-                    self.__hWnd, win32con.GWL_WNDPROC, self.__oldProc
-                )
-            except Exception:
-                pass  # Window already destroyed, nothing to restore
-            # Let Windows handle destroy with default proc
-            return win32gui.DefWindowProc(hWnd, msg, wParam, lParam)
-
-        if msg == win32con.WM_POWERBROADCAST:
-            if wParam == win32con.PBT_APMSUSPEND:
-                wx.CallAfter(self.OnPowerState, self.POWEROFF)
-            elif wParam == win32con.PBT_APMRESUMESUSPEND:
-                wx.CallAfter(self.OnPowerState, self.POWERON)
-
-        return win32gui.CallWindowProc(
-            self.__oldProc, hWnd, msg, wParam, lParam
-        )
+    def __onPowerResume(self, event):
+        """Handle system resume event."""
+        self.OnPowerState(self.POWERON)
+        event.Skip()
