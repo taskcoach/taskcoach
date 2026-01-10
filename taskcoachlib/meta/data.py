b52a80bb2733a6a7c0681d8dcc6971ed5db3e46b
Real Carbonneau <contact@realcarbonneau.com>
2026-01-10 10:42:28 -0500

    Bump version to 2.0.1.4

--- taskcoachlib/meta/data.py
+++ taskcoachlib/meta/data.py
@@ -40,10 +40,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # =============================================================================
 
 version = "2.0.1"  # Major.Minor.Milestone
-patch = "3"  # Patch number - INCREMENT THIS for each release
-version_full = f"{version}.{patch}"  # Full version: 2.0.1.3
+patch = "4"  # Patch number - INCREMENT THIS for each release
+version_full = f"{version}.{patch}"  # Full version: 2.0.1.4
 
-release_day = "9"  # Day of the release (1-31)
+release_day = "10"  # Day of the release (1-31)
 release_month = "January"  # Month of the release
 release_year = "2026"  # Year of the release
 
